24b5a959e47a4083ef2059312834798095e33ac0
Kelompokk7 <josuataberima007@gmail.com>
2023-12-24 19:35:46 +0700

    hapus routes getAll

--- router/payment.js
+++ router/payment.js
@@ -3,7 +3,6 @@ const paymentController = require("../controller/paymentController");
 const routePayment = express.Router();
 
 routePayment.post("/", paymentController.charge)
-routePayment.get("/get", paymentController.getAll)
 routePayment.post("/notifikasi", paymentController.notifikasi);
 
 
